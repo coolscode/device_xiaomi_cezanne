53f056a9b73b4af77995e00095fa5413f4e2f81c
Jarl-Penguin <jarlpenguin@outlook.com>
2021-08-31 18:50:04 +0530

    cezanne: Set MediaTek perf props
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I2febc9bcd6749e0e05ecb73433830591f1959187
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,9 @@
+# Performance
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.mtk_perf_simple_start_win=1 \
+    ro.mtk_perf_fast_start_win=1 \
+    ro.mtk_perf_response_time=1
+
 # Zygote
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
