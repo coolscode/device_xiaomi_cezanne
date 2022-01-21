17f0c6ed5d16ae505edf8c8e65cc774608178e9d
Arian <arian.kulmer@web.de>
2022-01-21 22:43:00 +0530

    cezanne: Provide patchself of vtservice with libsink
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: Ie3397cc90c741e7a711012a66fed3bf2ada6a818

--- extract-files.sh
+++ extract-files.sh
@@ -34,6 +34,14 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function blob_fixup {
+    case "$1" in
+        lib/libsink.so)
+            "$PATCHELF" --add-needed "libshim_vtservice.so" "$2"
+            ;;
+    esac
+}
+
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
