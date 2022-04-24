1e74352c7a9b03b18b2af46322dc8dde351864f8
AngelaCooljx <ljx15007902367@gmail.com>
2022-04-24 17:25:59 +0800

    Revert "cezanne: Update CameraLite to latest version"
    This reverts commit e81cf2d9b2338e19f4679a837813cf39d3b4ddce.

--- device.mk
+++ device.mk
@@ -52,7 +52,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    CameraLite
+    CameraGo
 
 PRODUCT_COPY_FILES += \
     hardware/interfaces/camera/provider/2.4/default/android.hardware.camera.provider@2.4-service_64.rc:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init/android.hardware.camera.provider@2.4-service_64.rc
