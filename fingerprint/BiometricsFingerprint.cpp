4ee53f8a5955cc4d425217c789adb2cc4b4f64fd
AngelaCooljx <ljx15007902367@gmail.com>
2022-05-17 02:40:24 +0800

    cezanne: Update udfps dimming

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -47,18 +47,15 @@ template <typename T>
 static void set(const std::string& path, const T& value) {
     std::ofstream file(path);
     file << value;
-    LOG(ERROR) << "Set " << path << value;
 }
 
 static void threadboost(sp<IXiaomiFingerprint> txiaomiFingerprintService){
-    LOG(ERROR) << "Thread start";
-    usleep(200000);
+    usleep(100000);
     set(DISPPARAM_PATH, DISPPARAM_HBM_UDFPS_ON);
     txiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_UDFPS);
 }
 
 BiometricsFingerprint::BiometricsFingerprint() {
-    LOG(ERROR) << "Start!";
     biometrics_2_1_service = IBiometricsFingerprint_2_1::getService();
     touchFeatureService = ITouchFeature::getService();
     xiaomiFingerprintService = IXiaomiFingerprint::getService();
@@ -68,7 +65,6 @@ BiometricsFingerprint::BiometricsFingerprint() {
 }
 
 Return<uint64_t> BiometricsFingerprint::setNotify(const sp<IBiometricsFingerprintClientCallback>& clientCallback) {
-    LOG(ERROR) << "setNotify()";
     return biometrics_2_1_service->setNotify(clientCallback);
 }
 
@@ -77,7 +73,6 @@ Return<uint64_t> BiometricsFingerprint::preEnroll() {
 }
 
 Return<RequestStatus> BiometricsFingerprint::enroll(const hidl_array<uint8_t, 69>& hat, uint32_t gid, uint32_t timeoutSec) {
-    LOG(ERROR) << "enroll()";
     xiaomiDisplayFeatureService->setFeature(0, 20, 0, 255);
     return biometrics_2_1_service->enroll(hat, gid, timeoutSec);
 }
@@ -96,32 +91,26 @@ Return<RequestStatus> BiometricsFingerprint::cancel() {
 }
 
 Return<RequestStatus> BiometricsFingerprint::enumerate() {
-    LOG(ERROR) << "enumerate()";
     return biometrics_2_1_service->enumerate();
 }
 
 Return<RequestStatus> BiometricsFingerprint::remove(uint32_t gid, uint32_t fid) {
-    LOG(ERROR) << "remove(" << gid << ", " << fid << ")" ;
     return biometrics_2_1_service->remove(gid, fid);
 }
 
 Return<RequestStatus> BiometricsFingerprint::setActiveGroup(uint32_t gid, const hidl_string& storePath) {
-    LOG(ERROR) << "setActiveGroup()";
     return biometrics_2_1_service->setActiveGroup(gid, storePath);
 }
 
 Return<RequestStatus> BiometricsFingerprint::authenticate(uint64_t operationId, uint32_t gid) {
-    LOG(ERROR) << "authenticate()";
     return biometrics_2_1_service->authenticate(operationId, gid);
 }
 
 Return<bool> BiometricsFingerprint::isUdfps(uint32_t) {
-    LOG(ERROR) << "isUdfps()";
     return true;
 }
 
-Return<void> BiometricsFingerprint::onFingerDown(uint32_t a, uint32_t b, float c, float d) {
-    LOG(ERROR) << "onFingerDown " << a << ", "  << b << ", " << c << ", " << d;
+Return<void> BiometricsFingerprint::onFingerDown(uint32_t, uint32_t, float, float) {
     std::thread(threadboost,xiaomiFingerprintService).detach();
     xiaomiDisplayFeatureService->setFeature(0, 20, 1, 255);
     xiaomiDisplayFeatureService->setFeature(0, 17, 1, 1);
@@ -130,7 +119,7 @@ Return<void> BiometricsFingerprint::onFingerDown(uint32_t a, uint32_t b, float c
 }
 
 Return<void> BiometricsFingerprint::onFingerUp() {
-    LOG(ERROR) << "onFingerUp()";
+    set(DISPPARAM_PATH, DISPPARAM_HBM_UDFPS_OFF);
     touchFeatureService->setTouchMode(TOUCH_FOD_ENABLE, 0);
     xiaomiDisplayFeatureService->setFeature(0, 17, 0, 1);
     return Void();
