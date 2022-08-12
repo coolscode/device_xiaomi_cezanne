58e6346db7e5e4480cef424704540162fc5816c8
AngelaCooljx <ljx15007902367@gmail.com>
2022-08-12 23:15:29 +0800

    cezanne: Update prebuilt kernel to MIUI-V13.0.3.0

--- aosp_cezanne.mk
+++ aosp_cezanne.mk
@@ -36,6 +36,6 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cezanne-user 12 SP1A.210812.016 V13.0.2.0.SJNCNXM release-keys"
+    PRIVATE_BUILD_DESC="cezanne-user 12 SP1A.210812.016 V13.0.3.0.SJNCNXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/cezanne/cezanne:12/SP1A.210812.016/V13.0.2.0.SJNCNXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/cezanne/cezanne:12/SP1A.210812.016/V13.0.3.0.SJNCNXM:user/release-keys
